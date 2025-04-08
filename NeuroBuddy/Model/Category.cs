8f5701c85788a4c0ae99640709a25da0a671d271
dejectedarcher <ahmedkk96@gmail.com>
2025-04-08 04:43:51 +0200

    Refactoring Category.cs

--- NeuroBuddy/Model/Category.cs
+++ NeuroBuddy/Model/Category.cs
@@ -4,14 +4,17 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace NeuroBuddy.Model
 {
     class Category
     {
         public string Name { get; set; }
-        public Category? RootCategory { get; set; }
-        bool isRoot => SubCategories.Count > 0;
+        public Category? RootCategory { get; private set;}
+        public bool HasChildren => SubCategories.Count > 0;
+
+        public bool HasRoot => RootCategory != null;
 
         public List<Category> SubCategories;
 
@@ -20,6 +23,27 @@ namespace NeuroBuddy.Model
             Name = name;
             SubCategories = new List<Category>();
             this.RootCategory = root;
+        }
+
+        public void AddChildCategory(Category child)
+        {
+            if (child.HasRoot)
+                throw new Exception("Trying to add a new child category which already has a root");
+
+            if (SubCategories.Contains(child))
+                throw new Exception("This category already contains this child category");
+
+            child.RootCategory = this;
+            SubCategories.Add(child);
+        }
+    
+        public void RemoveChild(Category child)
+        {
+            if (!SubCategories.Contains(child))
+                throw new Exception("Can't remove child category, it's not a child of this category");
+
+            SubCategories.Remove(child);
+            child.RootCategory = null;
         }
     }
 }
